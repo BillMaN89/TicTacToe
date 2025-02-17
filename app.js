3ff4d40c622c91bd919b6b05eca1fab286877a42
BillMaN89 <billman-synas@hotmail.com>
2025-02-17 17:03:06 +0200

    create GameController function, add CreatePlayer function

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function gameBoard(){
+function Gameboard(){
 //board initialization
     const rows = 3;
     const columns = 3;
@@ -22,6 +22,7 @@ function gameBoard(){
     }
 
     function placeMarker(row, col, marker) {
+        //move validation
         if (board[row][col].getValue() !== null){
             console.log("Cell already taken \n Please choose another one");
             return false;
@@ -48,3 +49,56 @@ function Cell(){
 
     return { getValue, setValue}
 }
+
+
+function CreatePlayer(name, marker){
+    return {name, marker}
+}
+
+function GameController(PlayerOneName, PlayerTwoName){
+    const PlayerOne = CreatePlayer(PlayerOneName, "X");
+    const PlayerTwo = CreatePlayer(PlayerTwoName, "O");
+
+    const board = Gameboard();
+
+    let activePlayer = PlayerOne;
+
+    const SwitchPlayerTurn = () =>{
+        activePlayer = activePlayer === PlayerOne ? PlayerTwo : PlayerOne;
+    }
+
+    const getActivePlayer = () => activePlayer;
+
+    const printNewRound = () =>{
+        board.printBoard();
+        console.log(`${getActivePlayer().name} 's turn`);
+    }
+
+    const playRound = (row, col) => {
+        //get active player's marker and place it on the board
+        const marker = getActivePlayer().marker;
+        const success = board.placeMarker(row, col, marker);
+
+        if (!success){
+            return;
+        }
+        //next Round
+        console.log(`${getActivePlayer().name} placed their marker at (${row}, ${col})`);
+
+        //winner validation
+        if (CheckWinner()){
+            console.log(`${getActivePlayer().name} wins the game!`);
+            return;
+        }
+
+        SwitchPlayerTurn();
+        printNewRound();
+    };
+
+    //Initial Round message
+    printNewRound();
+    
+    return {playRound, getActivePlayer};
+}
+
+const game = GameController("Alice", "Bill");
